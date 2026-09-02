27e66c0d5079e4d7743b34093170647d0d0c84ae
Pancraciolego <154642672+Pancraciolego@users.noreply.github.com>
2026-09-02 11:04:58 -0800

    yaa
    ya

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Raziel2.kt
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode
+
+class Raziel2 {
+
+    var age = 17
+    var height_inches = 66
+    var creepiness = true
+    val name = "Raziel"
+
+    fun changeCreepiness(){
+        creepiness = false
+    }
+
+}
